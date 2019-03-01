bfebd68f1806582ff7709ed15d35e55af7230cf2
gackey.21 <gackey.21@gmail.com>
2019-03-01 09:45:01 +0900

    initial　試用

--- plugin.php
+++ plugin.php
@@ -32,9 +32,11 @@ if ( ! class_exists( 'thx_Cocoon_Option' ) ) {
 	class thx_Cocoon_Option {
 		public function __construct() {
 			require_once( __DIR__.'/src/hsla.php' );//hsla変調
+			require_once( __DIR__.'/src/is-mobile.php' );//スマホ判別
 		}//__construct()
 
 		static $_var = __DIR__.'/src/_var.php';//変数ファイル
+		static $src = __DIR__.'/src/';
 
 		//ファイル書き出し
 		public function str_to_file($path, $str) {
@@ -46,5 +48,28 @@ if ( ! class_exists( 'thx_Cocoon_Option' ) ) {
 		}
 	}//class
 }//! class_exists
+//設定変更CSSを読み込む
+if ( !function_exists( 'wp_add_css_custome_to_inline_style' ) ):
+function wp_add_css_custome_to_inline_style(){
+	ob_start();//バッファリング
+	get_template_part('tmp/css-custom');
+	require( thx_Cocoon_Option::$_var );
+	require_once( thx_Cocoon_Option::$src.'initial.php' );
+	require_once( thx_Typography::$css_amp_dir.'_typography.php' );
+	require_once( thx_Typography::$css_amp_dir.'amp.php' );
+	require_once( thx_Typography::$css_amp_dir.'h.php' );
+	$css_custom = ob_get_clean();
+	//CSSの縮小化
+	$css_custom = minify_css($css_custom);
+	//HTMLにインラインでスタイルを書く
+	if (get_skin_url()) {
+		//スキンがある場合
+		wp_add_inline_style( THEME_NAME.'-skin-style', $css_custom );
+	} else {
+		//スキンを使用しない場合
+		wp_add_inline_style( THEME_NAME.'-style', $css_custom );
+	}
+}
+endif;//!function_exists( 'wp_add_css_custome_to_inline_style' )
 
 new thx_Cocoon_Option;
