b79fa35927d90fbfa024ec883cf971c1c239e219
gackey.21 <gackey.21@gmail.com>
2019-03-03 12:16:58 +0900

    画像の囲み効果　追加

--- src/initial.php
+++ src/initial.php
@@ -272,3 +272,34 @@ pre code span {
 	word-wrap: normal;
 	overflow-wrap: normal;
 }
+<?php
+///////////////////////////////////////
+// 画像の囲み効果
+?>
+/*ボーダー*/
+.iwe-border .wp-block-image img, .iwe-border .wp-block-image amp-img,
+.iwe-border .wp-block-gallery img,
+.iwe-border .wp-block-gallery amp-img {
+	border: 1px solid #ccc;
+}
+
+/*ボーダー（太線）*/
+.iwe-border-bold .wp-block-image img, .iwe-border-bold .wp-block-image amp-img,
+.iwe-border-bold .wp-block-gallery img,
+.iwe-border-bold .wp-block-gallery amp-img {
+	border: 4px solid #eee;
+}
+
+/*シャドー*/
+.iwe-shadow .wp-block-image img, .iwe-shadow .wp-block-image amp-img,
+.iwe-shadow .wp-block-gallery img,
+.iwe-shadow .wp-block-gallery amp-img {
+	box-shadow: 5px 5px 15px #ddd;
+}
+
+/*シャドーペーパー*/
+.iwe-shadow-paper .wp-block-image img, .iwe-shadow-paper .wp-block-image amp-img,
+.iwe-shadow-paper .wp-block-gallery img,
+.iwe-shadow-paper .wp-block-gallery amp-img {
+	box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);
+}
