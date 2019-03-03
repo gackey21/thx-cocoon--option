f3cc497c4dc7c47fa2192dd2612d2ff2ac034e0b
gackey.21 <gackey.21@gmail.com>
2019-03-03 12:13:15 +0900

    pre　追加

--- src/initial.php
+++ src/initial.php
@@ -253,3 +253,22 @@ li ul li:before {
 	font-family: FontAwesome;
 	content: "\f105";
 }
+<?php
+///////////////////////////////////////
+// pre
+?>
+pre {
+	word-wrap: normal;
+	overflow-wrap: normal;
+	word-break: normal;
+}
+pre code {
+	word-wrap: normal;
+	overflow-wrap: normal;
+	font-size: 1rem;
+	line-height: 1rem;
+}
+pre code span {
+	word-wrap: normal;
+	overflow-wrap: normal;
+}
