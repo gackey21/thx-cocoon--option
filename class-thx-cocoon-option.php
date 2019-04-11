4d5aa618a7757a850a529660d3a675d3487279b8
gackey.21 <gackey.21@gmail.com>
2019-04-11 15:45:33 +0900

    Beautify

--- class-thx-cocoon-option.php
+++ class-thx-cocoon-option.php
@@ -26,11 +26,15 @@ License: GPL2
 		Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 ?>
-<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;}
+?>
 <?php
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-if ( ! is_plugin_active( 'thx-customize-core/plugin.php' ) ): ?>
-<?php
+if ( ! is_plugin_active( 'thx-customize-core/plugin.php' ) ) :
+	?>
+	<?php
 	add_action( 'admin_notices', 'thx_Cocoon_Option_admin_notices' );
 	function thx_Cocoon_Option_admin_notices() {
 		?>
@@ -41,130 +45,130 @@ if ( ! is_plugin_active( 'thx-customize-core/plugin.php' ) ): ?>
 		</div>
 		<?php
 		deactivate_plugins( plugin_basename( __FILE__ ) );
-		unset( $_GET["activate"] );
+		unset( $_GET['activate'] );
 	}
-?>
-<?php else://! is_plugin_active( 'thx-customize-core/plugin.php' ) ?>
-<?php
-if ( ! class_exists( 'thx_Cocoon_Option' ) ) {
-	class thx_Cocoon_Option {
-		//読み込むurl
-		static $push_css_dir = array();
-		static $push_amp_dir = array();
-		static $replace_cocoon_css;
-		static $replace_cocoon_amp;
-		static $css_dir = __DIR__.'/src/css/';
-		static $dest_dir = __DIR__.'/dest/';
+	?>
+<?php else : //! is_plugin_active( 'thx-customize-core/plugin.php' ) ?>
+	<?php
+	if ( ! class_exists( 'thx_Cocoon_Option' ) ) {
+		class thx_Cocoon_Option {
+			//読み込むurl
+			static $push_css_dir = array();
+			static $push_amp_dir = array();
+			static $replace_cocoon_css;
+			static $replace_cocoon_amp;
+			static $css_dir  = __DIR__ . '/src/css/';
+			static $dest_dir = __DIR__ . '/dest/';
 
-		public function __construct() {
-			$thx_co_option = get_option('thx_co_option');
+			public function __construct() {
+				$thx_co_option = get_option( 'thx_co_option' );
 
-			//管理画面の設定
-			add_action('admin_menu', array($this, 'add_sub_menu'));
-			add_action('admin_init', 'thx_cocoon_option_settings_init');
+				//管理画面の設定
+				add_action( 'admin_menu', array( $this, 'add_sub_menu' ) );
+				add_action( 'admin_init', 'thx_cocoon_option_settings_init' );
 
-			//プラグインメニューの設定
-			add_filter(
-				'plugin_action_links_' . plugin_basename( __FILE__ ),
-				array( $this, 'add_action_links' )
-			);
+				//プラグインメニューの設定
+				add_filter(
+					'plugin_action_links_' . plugin_basename( __FILE__ ),
+					array( $this, 'add_action_links' )
+				);
 
-			//アンインストール
-			if(function_exists('register_uninstall_hook')) {
-				register_uninstall_hook (__FILE__, 'thx_Cocoon_Option::thx_co_uninstall');
+				//アンインストール
+				if ( function_exists( 'register_uninstall_hook' ) ) {
+					register_uninstall_hook( __FILE__, 'thx_Cocoon_Option::thx_co_uninstall' );
+				}
+
+				//親amp.cssの変更＆出力
+				add_filter( 'amp_parent_css', array( 'thx_Cocoon_amp', 'echo_amp_parent_css' ) );
+				//カスタマイズampの出力
+				add_filter( 'amp_all_css', array( 'thx_Cocoon_amp', 'echo_amp_all_css' ) );
+
+				//追加関数の読み込み
+				require_once( __DIR__ . '/src/php/hsla.php' );//hsla変調
+				require_once( __DIR__ . '/src/php/is-mobile.php' );//スマホ判別
+
+				//エントリーカード・サムネイルの余白を均等にする
+				if ( $thx_co_option['equalizing_thumbnail_margin_in_entry_card']['amp'] == 1 ) {
+					$this::$push_amp_dir[] = $this::$css_dir . 'entry-card-margin.php';
+				}
+				if ( $thx_co_option['equalizing_thumbnail_margin_in_entry_card']['style'] == 1 ) {
+					$this::$push_css_dir[] = $this::$css_dir . 'entry-card-margin.php';
+				}
+
+				//カードをホバー（COLORS風）
+				if ( $thx_co_option['wrap_hover']['amp'] == 1 ) {
+					$this::$push_amp_dir[] = $this::$css_dir . 'wrap-hover.php';
+				}
+				if ( $thx_co_option['wrap_hover']['style'] == 1 ) {
+					$this::$push_css_dir[] = $this::$css_dir . 'wrap-hover.php';
+				}
+
+				//画像の囲み効果（ブロックエディタ）
+				if ( $thx_co_option['iwe_block_editor']['amp'] == 1 ) {
+					$this::$push_amp_dir[] = $this::$css_dir . 'iwe-amp.php';
+				}
+				if ( $thx_co_option['iwe_block_editor']['style'] == 1 ) {
+					$this::$push_css_dir[] = $this::$css_dir . 'iwe-css.php';
+				}
+
+				//ハイライト表示を横スクロールに変更
+				if ( $thx_co_option['pre']['amp'] == 1 ) {
+					$this::$push_amp_dir[] = $this::$css_dir . 'pre.php';
+				}
+				if ( $thx_co_option['pre']['style'] == 1 ) {
+					$this::$push_css_dir[] = $this::$css_dir . 'pre.php';
+				}
+
+				//phpでcssを記述
+				if ( $thx_co_option['php_css']['amp'] == 1 ) {
+					$this::$push_amp_dir[] = $this::$dest_dir . 'thx-phped.css';
+				}
+				if ( $thx_co_option['php_css']['style'] == 1 ) {
+					$this::$push_css_dir[] = $this::$dest_dir . 'thx-phped.css';
+				}
+
+				//親スタイルの変更
+				if ( $thx_co_option['replace_cocoon_style']['amp'] == 1 ) {
+					$this::$replace_cocoon_amp = $thx_co_option['replace_cocoon_style_array'];
+				}
+				if ( $thx_co_option['replace_cocoon_style']['style'] == 1 ) {
+					$this::$replace_cocoon_css = $thx_co_option['replace_cocoon_style_array'];
+				}
+
+				// add_action('wp_enqueue_scripts', array($this, 'push_url'));
+			}//__construct()
+
+			static $_var = __DIR__ . '/src/css/_var.php';//変数ファイル
+			static $src  = __DIR__ . '/src/';
+
+			//アインインストール時にオプション削除
+			private static function thx_co_uninstall() {
+				delete_option( 'thx_co_option' );
 			}
 
-			//親amp.cssの変更＆出力
-			add_filter('amp_parent_css', array( 'thx_Cocoon_amp', 'echo_amp_parent_css' ));
-			//カスタマイズampの出力
-			add_filter('amp_all_css', array( 'thx_Cocoon_amp', 'echo_amp_all_css' ));
-
-			//追加関数の読み込み
-			require_once( __DIR__.'/src/php/hsla.php' );//hsla変調
-			require_once( __DIR__.'/src/php/is-mobile.php' );//スマホ判別
-
-			//エントリーカード・サムネイルの余白を均等にする
-			if ($thx_co_option['equalizing_thumbnail_margin_in_entry_card']['amp'] == 1) {
-				$this::$push_amp_dir[] = $this::$css_dir.'entry-card-margin.php';
-			}
-			if ($thx_co_option['equalizing_thumbnail_margin_in_entry_card']['style'] == 1) {
-				$this::$push_css_dir[] = $this::$css_dir.'entry-card-margin.php';
+			//設定リンク追加
+			static function add_action_links( $links ) {
+				$add_link = '<a href="admin.php?page=thx-jp-cocoon-option">設定</a>';
+				array_unshift( $links, $add_link );
+				return $links;
 			}
 
-			//カードをホバー（COLORS風）
-			if ($thx_co_option['wrap_hover']['amp'] == 1) {
-				$this::$push_amp_dir[] = $this::$css_dir.'wrap-hover.php';
+			//サブメニュー作成
+			static function add_sub_menu() {
+				add_submenu_page(
+					'thx-jp-customize-core',
+					'thx.jp/ Cocoon Option の設定',
+					'Cocoon Option',
+					'manage_options',
+					'thx-jp-cocoon-option',
+					'thx_cocoon_option_form'
+				);
 			}
-			if ($thx_co_option['wrap_hover']['style'] == 1) {
-				$this::$push_css_dir[] = $this::$css_dir.'wrap-hover.php';
-			}
+		}//class
+	}//! class_exists
 
-			//画像の囲み効果（ブロックエディタ）
-			if ($thx_co_option['iwe_block_editor']['amp'] == 1) {
-				$this::$push_amp_dir[] = $this::$css_dir.'iwe-amp.php';
-			}
-			if ($thx_co_option['iwe_block_editor']['style'] == 1) {
-				$this::$push_css_dir[] = $this::$css_dir.'iwe-css.php';
-			}
-
-			//ハイライト表示を横スクロールに変更
-			if ($thx_co_option['pre']['amp'] == 1) {
-				$this::$push_amp_dir[] = $this::$css_dir.'pre.php';
-			}
-			if ($thx_co_option['pre']['style'] == 1) {
-				$this::$push_css_dir[] = $this::$css_dir.'pre.php';
-			}
-
-			//phpでcssを記述
-			if ($thx_co_option['php_css']['amp'] == 1) {
-				$this::$push_amp_dir[] = $this::$dest_dir.'thx-phped.css';
-			}
-			if ($thx_co_option['php_css']['style'] == 1) {
-				$this::$push_css_dir[] = $this::$dest_dir.'thx-phped.css';
-			}
-
-			//親スタイルの変更
-			if ($thx_co_option['replace_cocoon_style']['amp'] == 1) {
-				$this::$replace_cocoon_amp = $thx_co_option['replace_cocoon_style_array'];
-			}
-			if ($thx_co_option['replace_cocoon_style']['style'] == 1) {
-				$this::$replace_cocoon_css = $thx_co_option['replace_cocoon_style_array'];
-			}
-
-			// add_action('wp_enqueue_scripts', array($this, 'push_url'));
-		}//__construct()
-
-		static $_var = __DIR__.'/src/css/_var.php';//変数ファイル
-		static $src = __DIR__.'/src/';
-
-		//アインインストール時にオプション削除
-		private static function thx_co_uninstall() {
-			delete_option('thx_co_option');
-		}
-
-		//設定リンク追加
-		static function add_action_links ( $links ) {
-			$add_link = '<a href="admin.php?page=thx-jp-cocoon-option">設定</a>';
-			array_unshift( $links, $add_link);
-			return $links;
-		}
-
-		//サブメニュー作成
-		static function add_sub_menu() {
-			add_submenu_page(
-				'thx-jp-customize-core',
-				'thx.jp/ Cocoon Option の設定',
-				'Cocoon Option',
-				'manage_options',
-				'thx-jp-cocoon-option',
-				'thx_cocoon_option_form'
-			);
-		}
-	}//class
-}//! class_exists
-
-require_once('src/php/menu.php');
-require_once('src/php/cocoon-style.php');
+	require_once( 'src/php/menu.php' );
+	require_once( 'src/php/cocoon-style.php' );
 
 	// ヘッダー部の書き換えβ
 	// $tmp_php = thx_Customize_Core::file_to_str(__DIR__.'/../../src/child/tmp/header-container.php');
@@ -174,7 +178,7 @@ require_once('src/php/cocoon-style.php');
 	// $path = get_stylesheet_directory().'/tmp/css-custom.php';
 	// $tcc -> str_to_file($path, $tmp_php);
 
-new thx_Cocoon_Option;
+	new thx_Cocoon_Option;
 endif;//! is_plugin_active( 'thx-customize-core/plugin.php' )
 
 // ampの作られ方
